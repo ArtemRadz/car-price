8b50dacbe9bbb5475f1176f744d30888699e1e10
artemradz <artemradzivon@gmail.com>
2024-04-02 17:41:32 +0300

    added create user logic for DB

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dtos/create-user.dto';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
+  ) {}
+
+  create(body: CreateUserDto) {
+    const user = this.usersRepository.create(body);
+
+    return this.usersRepository.save(user);
+  }
+}
